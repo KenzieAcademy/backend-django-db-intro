ae86a01db05f41ccb9152232d5cae92cf597a6dc
infosmith <dst.dsmith@gmail.com>
2019-04-01 14:47:07 -0400

    feat: add test database configuration
    Why:
    
    * Isolate test databases and allow it to be preserved with --keepdb
    
    This change addresses the need by:
    
    * Configure Django to replicate and isolate test database

--- db_demo/settings.py
+++ db_demo/settings.py
@@ -78,9 +78,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'TEST': {
+            'NAME': os.path.join(BASE_DIR, 'test.sqlite3'),
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
