8f8ab1927bc6beb3f1d88cb46fbbb5bf755535f0
infosmith <dst.dsmith@gmail.com>
2019-04-01 14:37:47 -0400

    fix: order imports after docstrings
    Why:
    
    * Docstrings come before imports, even single line comments serving as docstrings.
    
    This change addresses the need by:
    
    * Reorder import after docstring

--- db_demo/models.py
+++ db_demo/models.py
@@ -1,7 +1,9 @@
-from django.db import models
-
+# The built-in User model already has secure handling for things like
+# username, password, email address, and so on.
 # The built-in User model already has secure handling for things like
 # username, password, email address, and so on.
 from django.contrib.auth.models import User
+from django.db import models
+
 
 # Your models go here!
